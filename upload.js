5321404f02569f6618fbf953e566ff99afbc5f20
czani89 <czani89@gmail.com>
2021-06-23 12:02:31 +0200

    first commit

---
+++ upload.js
@@ -0,0 +1,7 @@
+const upload = document.querySelector(".upload");
+const cancel = document.querySelector(".cancel"); 
+const replace = document.querySelector(".replace"); 
+const file = document.querySelector(".file");
+
+
+
